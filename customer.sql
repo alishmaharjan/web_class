32b754fc1f27786b6c0b49a82970641215756f63
alishmaharjan <108648849+alishmaharjan@users.noreply.github.com>
2022-11-16 11:30:22 +0545

    Add files via upload

--- customer.sql
+++ customer.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 02, 2022 at 06:50 AM
+-- Generation Time: Nov 16, 2022 at 06:27 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `first normal form`
+-- Database: `third normal form`
 --
 
 -- --------------------------------------------------------
@@ -31,27 +31,8 @@ CREATE TABLE `customer` (
   `Membership_id` int(11) NOT NULL,
   `Full_Name` varchar(255) NOT NULL,
   `Physical_Address` varchar(255) NOT NULL,
-  `Salutation` varchar(255) NOT NULL
+  `Salutation` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `customer`
---
-
-INSERT INTO `customer` (`Membership_id`, `Full_Name`, `Physical_Address`, `Salutation`) VALUES
-(1, 'Jane Jones', 'First Street Plot No 4', 'Ms'),
-(2, 'Robert Phill', '3rd Street 34', 'Mr'),
-(3, 'Robert Phill', '5th Avenue', 'Mr');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `customer`
---
-ALTER TABLE `customer`
-  ADD PRIMARY KEY (`Membership_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ office.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 16, 2022 at 06:28 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `office`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bonus`
+--
+
+CREATE TABLE `bonus` (
+  `WORKER_REF_ID` int(11) DEFAULT NULL,
+  `BONUS_AMMOUNT` int(10) DEFAULT NULL,
+  `BONUS_DATE` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bonus`
+--
+
+INSERT INTO `bonus` (`WORKER_REF_ID`, `BONUS_AMMOUNT`, `BONUS_DATE`) VALUES
+(1, 5, '2022-10-21 10:46:19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `worker`
+--
+
+CREATE TABLE `worker` (
+  `Worker_id` int(11) NOT NULL,
+  `FIRST_NAME` char(25) DEFAULT NULL,
+  `LAST_NAME` char(25) DEFAULT NULL,
+  `SALARY_INT` int(15) DEFAULT NULL,
+  `JOINING_DATE` datetime DEFAULT NULL,
+  `DEPARTMENT` char(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `worker`
+--
+
+INSERT INTO `worker` (`Worker_id`, `FIRST_NAME`, `LAST_NAME`, `SALARY_INT`, `JOINING_DATE`, `DEPARTMENT`) VALUES
+(0, '[value-2]', '[value-3]', 0, '0000-00-00 00:00:00', '[value-6]'),
+(1, 'sudat', 'shrestha', 1, '0000-00-00 00:00:00', 'god'),
+(2, 'alish', 'maharjan', 1, '0000-00-00 00:00:00', 'manager');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bonus`
+--
+ALTER TABLE `bonus`
+  ADD KEY `WORKER_REF_ID` (`WORKER_REF_ID`);
+
+--
+-- Indexes for table `worker`
+--
+ALTER TABLE `worker`
+  ADD PRIMARY KEY (`Worker_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `worker`
+--
+ALTER TABLE `worker`
+  MODIFY `Worker_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bonus`
+--
+ALTER TABLE `bonus`
+  ADD CONSTRAINT `bonus_ibfk_1` FOREIGN KEY (`WORKER_REF_ID`) REFERENCES `worker` (`Worker_id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
