672c973cd8d461da042018fc3290db062a51afdb
alishmaharjan <108648849+alishmaharjan@users.noreply.github.com>
2022-11-24 10:32:42 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+//This is a variable
+#this is a variable
+$i = 10;
+$y = 5;
+$d = "The sum of $i and $y is" ;
+$s = "The multiple of $i and $y is" ;
+$c = "The difference between $i and $y is";
+echo $x , " " , $d , " " , $i + $y, " ", $s , " ", $i*$y, $c , " ", $i-$y;
+?>
+
+
+
+
+
